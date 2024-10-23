f595916096d16ec5e84660b2a23232644c2dd92c
Pablo Lires <pablo@librecomunicacion.net>
2024-10-23 19:49:57 -0300

    modificacion de page 404

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,8 +1,20 @@
+import { Link } from 'react-router-dom'
+import styles from './not-found.module.css'
+
 const NotFound = () => {
   return (
-    <>
-      <h2>NotFound</h2>
-    </>
+    <main className={`${styles.section_not_found}`}>
+      <h1>Error 404</h1>
+      <p>
+        Llegaste aquí buscando algo que ya no está. <br />
+        contactate con nosotros y te ayudaremos a encontrarlo
+      </p>
+      <div className='content_btn'>
+        <Link to='/' className={`btn ${styles.btnNotFound}`}>
+          Seguir Navegando
+        </Link>
+      </div>
+    </main>
   )
 }
 
